1e5874771ff6f47e3d65e8b9bf34fb685b7600db
Rafael Ribeiro Ramires da Silva <rafael.ribeiro.silva@gmail.com>
2022-05-29 18:52:36 -0300

    Criando rotas e controllers para testa algumas views

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,6 +2,7 @@ const usuarios = require('../database/usuarios.json');
 const fs = require('fs');
 const path = require('path');
 const bcrypt = require('bcrypt');
+const { userInfo } = require('os');
 
 const indexController = {
 
@@ -31,15 +32,30 @@ const indexController = {
 
   acaoCadastrar: (req, res) => {
     const { nome, email, senha } = req.body;
+
     const senhaEnc = bcrypt.hashSync(senha, 10);
 
-    const usuario = { nome, email, senha: senhaEnc };
+    let id = usuarios[usuarios.length -1].id +1;
+    
+    const usuario = { id, nome, email, senha: senhaEnc };
     
     usuarios.push(usuario);
 
     fs.writeFileSync(path.join('database', 'Usuarios.json'), JSON.stringify(usuarios));
 
     res.redirect('login');
+  },
+
+  painelUsuario: (req, res) => {
+    res.render('painel-usuario');
+  },
+
+  painelUsuarioEnderecos: (req, res) => {
+    res.render('painel-usuario-enderecos');
+  },
+
+  painelUsuarioPedidos: (req, res) => {
+    res.render('painel-usuario-pedidos');
   }
 }
 
