c2d67b1d0b1db732b2d431f1a8f871ec9af15527
Rafael Ribeiro Ramires da Silva <rafael.ribeiro.silva@gmail.com>
2022-06-08 16:02:53 -0300

    Reparo nas rotas

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var IndexRouter = require('./routes/IndexRouter');
 var AdmRouter = require('./routes/AdmRouter');
+var UsuarioController = require('./routes/UsuarioRouter')
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', IndexRouter);
 app.use('/adm', AdmRouter);
+app.use('/usuario', UsuarioController);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/UsuarioRouter.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var router = express.Router();
+var UsuarioController = require('../controllers/UsuarioController');
+
+module.exports = router;
