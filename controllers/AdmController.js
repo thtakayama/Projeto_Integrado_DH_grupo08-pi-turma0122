a5be7329b7c84c619fa0d375eaecb8a943a50a16
thtakayama <th.takayama58@gmail.com>
2022-06-18 13:07:17 -0300

    Ajustes Painel Administrador - Produtos

--- controllers/AdmController.js
+++ controllers/AdmController.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const bcrypt = require('bcrypt');
 const { userInfo } = require('os');
+const { uuid } = require('uuidv4');
+
+const produtos = [];
 
 module.exports = {
     
@@ -8,9 +11,29 @@ module.exports = {
     res.render('adm/login')
   },
   produtos: (req,res) => {
-    res.render('adm/produtos')
+    res.render('adm/produtos', {
+      produtos: produtos
+    });
   },
   produtosCadastrar: (req,res) => {
     res.render('adm/produtosCadastrar')
+  },
+  acaoCadastrarProduto: (req,res) => {
+    const titulo = req.body.titulo; 
+    const autor = req.body.autor; 
+    const preco = req.body.preco; 
+    const descricao = req.body.descricao; 
+    const avaliacao = req.body.avaliacao; 
+
+    const objProduto = {
+      titulo: titulo,
+      autor: autor,
+      preco: preco,
+      descricao: descricao,
+      avaliacao: avaliacao
+    }
+    produtos.push(objProduto);
+
+    res.redirect('/adm/produtos');
   }
 }
--- routes/AdmRouter.js
+++ routes/AdmRouter.js
@@ -4,6 +4,7 @@ var AdmController = require('../controllers/AdmController');
 
 router.get('/login', AdmController.login);
 router.get('/produtos', AdmController.produtos);
-router.get('/produtos/cadastrar', AdmController.produtosCadastrar);
+router.get('/cadastrar-produtos', AdmController.produtosCadastrar);
+router.post('/acao-cadastrar-produtos', AdmController.acaoCadastrarProduto);
 
 module.exports = router;
